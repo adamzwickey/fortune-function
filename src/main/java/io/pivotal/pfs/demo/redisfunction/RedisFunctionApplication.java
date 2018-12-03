b6cc149b034dc1bb90e07c44fc32fc7f441385b9
azwickey-pivotal <azwickey@pivotal.io>
2018-12-03 12:08:01 -0500

    added redis scaffolding

--- src/main/java/io/pivotal/pfs/demo/redisfunction/RedisFunctionApplication.java
+++ src/main/java/io/pivotal/pfs/demo/redisfunction/RedisFunctionApplication.java
@@ -1,5 +1,8 @@
 package io.pivotal.pfs.demo.redisfunction;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +16,20 @@ public class RedisFunctionApplication {
 		SpringApplication.run(RedisFunctionApplication.class, args);
 	}
 
+	private static Logger LOG = LoggerFactory.getLogger(RedisFunctionApplication.class);
+	@Autowired private FortuneRepository _repository;
+
 	@Bean
 	public Function<Long,Fortune> getFortune() {
-		return f ->  new Fortune("People are naturally attracted to you.");
+		return f ->  {
+			if(f == null) {
+				LOG.info("Retrieving Random fortune");
+				return new Fortune("People are naturally attracted to you.");
+			} else {
+				LOG.info("Retrieving Fortune: " + f);
+				return new Fortune("People are naturally attracted to you.");
+			}
+		};
 	}
 
 	@Bean
